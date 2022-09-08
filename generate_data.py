643b29515a65c68cb715c038dc284ea9f557a6f0
Alexandre Schulz <a.schulz1992@gmail.com>
2022-09-08 16:29:21 +0200

    still cleanup

--- generate_data.py
+++ generate_data.py
@@ -27,21 +27,23 @@ def generate_dataset(scip_parameters,path = "DataSet/",nb_cons = [500],nb_var = 
         print("Generate with Row:%d,Col:%d,Density:%f" % (row,col,d))
         for n in trange(1,nb_instance+1):
             problem_name = "set_cover_{"+row.__str__()+"*"+col.__str__()+"_"+d.__str__()+"_"+n.__str__()+"}"
+            problem_path = os.path.join(path, problem_name)
 
             done = False
             while(not done):
                 try:
                     instance = next(setCover)
                 except Exception as e:
-                    print(f"Exception occured : {e}")
+                    print(f"Failed to get next instance for params ({row, col, d}): {e}")
                     done = True
+                    break
 
                 if only_problem:
-                    instance.write_problem(path+problem_name+".lp")
+                    instance.write_problem(problem_path+".lp")
                 else:
                     #save problm lp
                     os.mkdir(path+problem_name)
-                    instance.write_problem(path+problem_name+"/problem.lp")
+                    instance.write_problem(os.path.join(problem_path,"problem.lp"))
 
                     #save features
                     obs, _, _, _, _ = env.reset(instance)
@@ -50,36 +52,30 @@ def generate_dataset(scip_parameters,path = "DataSet/",nb_cons = [500],nb_var = 
                         continue
                     if obs.row_features.shape[0] != row:
                         done = True
-                        if os.path.exists(path+problem_name):
+                        if os.path.exists(problem_path):
                             print(f"Removing : {path+problem_name}")
-                            shutil.rmtree(path+problem_name)
+                            shutil.rmtree(problem_path)
 
                         continue
                     #print("created pb")
                     #save constraintes features
-                    dumpRowFeatures(path+problem_name+"/constraints_features.json",obs.row_features)
+                    dumpRowFeatures(os.path.join(problem_path+"constraints_features.json"),obs.row_features)
                     #save variables features
-                    dumpVariableFeatures(path+problem_name+"/variables_features.json",obs.variable_features)
+                    dumpVariableFeatures(os.path.join(problem_path,"variables_features.json"),obs.variable_features)
                     #save edges features
                     original_indice = obs.variable_features[:,-1]
-                    dumpEdgeFeatures(path+problem_name+"/edges_features.json",obs.edge_features,original_indice)
+                    dumpEdgeFeatures(os.path.join(problem_path,"edges_features.json"),obs.edge_features,original_indice)
                     #get et save label
                     #print("saved features")
-                    solver = ecole.scip.Model.from_file(path+problem_name+"/problem.lp")
+                    solver = ecole.scip.Model.from_file(os.path.join(problem_path,"problem.lp"))
                     aspyscip = solver.as_pyscipopt()
                     aspyscip.setPresolve(pyscip.SCIP_PARAMSETTING.OFF)
                     aspyscip.optimize()
                     gapList.append(aspyscip.getGap())
-                    dumpSolution_Ecole(path+problem_name+"/label.json",aspyscip)
+                    dumpSolution_Ecole(os.path.join(problem_path,"label.json"),aspyscip)
                     
                 done = True             
 
-                #except Exception as ex:
-                #    print(row, col, d, "Erreur:%s"%ex)
-                #    done = True#False
-                #    if os.path.exists(path+problem_name):
-                #        print(f"Removing : {path+problem_name}")
-                #        shutil.rmtree(path+problem_name)
     gap = np.array(gapList)
     return np.mean(gap)
 
@@ -100,7 +96,7 @@ if __name__=="__main__":
 
     generate_dataset(scip_params, 
             path = "DataSet/",
-            nb_cons=[100,200,300,400,500], 
-            nb_var=[100,200,300], 
-            density=[0.1,0.15,0.2]
+            nb_cons=[100], 
+            nb_var=[100], 
+            density=[0.1]
             )
